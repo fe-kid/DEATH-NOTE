6530d6294a174f22f8825e7051fa86c6f71450a5
wisemett <frontendkid@gmail.com>
2021-08-25 16:00:37 +0900

    feat: Add global style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,19 @@
 import { Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './state/store';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import GameSelection from './pages/GameSelectino';
-import GamePlaying from './pages/GamePlaying';
-import Social from './pages/Social';
-import Ranks from './pages/Ranks';
-import Header from './components/Header';
+import { store } from '../state/store';
+import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
+import GameSelection from '../pages/GameSelectino';
+import GamePlaying from '../pages/GamePlaying';
+import Social from '../pages/Social';
+import Ranks from '../pages/Ranks';
+import Header from '../components/Header';
+import GlobalStyle from '../styles/globalStyle';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
