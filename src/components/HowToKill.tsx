7944d1f60ea85c7eaaab1b269c213a5a51fe8e01
wisemett <frontendkid@gmail.com>
2021-08-26 11:59:52 +0900

    feat: Add contents and styling to KillingStart page

--- src/components/HowToKill.tsx
+++ src/components/HowToKill.tsx
@@ -1,5 +1,52 @@
+import styled from 'styled-components';
+
+const StyledHowToKill = styled.div`
+  background-color: #444;
+  color: #fff;
+  margin: 50px;
+  padding: 30px;
+  h1 {
+    font-size: 40px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  ul {
+    li {
+      font-size: 20px;
+      margin-bottom: 10px;
+      text-shadow: 1px 2px 5px rgba(200, 200, 200, 0.2);
+    }
+  }
+`;
+
 const HowToKill = () => {
-  return <h1>How To Kill</h1>;
+  return (
+    <StyledHowToKill>
+      <h1>Instruction</h1>
+      <ul>
+        <li>
+          - You will see a screen displays criminals' information and what they
+          did.
+        </li>
+        <li>
+          - As a Kira, write their names on a page of your Death Note on the
+          bottom. It's case-sensitive.
+        </li>
+        <li>
+          - If you can't write a name, just click the skip button then punish
+          next one.
+        </li>
+        <li>
+          - If you write a wrong name, there will be a screen blocking for 2
+          seconds.
+        </li>
+        <li>
+          - You have 60 seconds. Let's see how many criminals you can punish in
+          a minute.
+        </li>
+      </ul>
+    </StyledHowToKill>
+  );
 };
 
 export default HowToKill;
--- src/components/KillingStartButton.tsx
+++ src/components/KillingStartButton.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const StyledButton = styled(Link)`
+  background-color: #222;
+  border: 4px solid #ccc;
+  border-radius: 20px;
+  display: block;
+  width: 200px;
+  padding: 10px 20px;
+  text-align: center;
+  margin: 0 auto;
+  font-size: 24px;
+  color: #eee;
+  text-decoration: none;
+`;
 const KillingStartButton = () => {
-  return <Link to="/killing">Start Killing</Link>;
+  return <StyledButton to="/killing">Start Working</StyledButton>;
 };
 
 export default KillingStartButton;
