0bb74135e8fd0f0ef9415e0e0270916eb183220a
wisemett <frontendkid@gmail.com>
2021-08-28 22:06:33 +0900

    feat: Implement SPA with GitHub pages

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/DEATH-NOTE">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
